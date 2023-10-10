6929f031ffd6460191b8723f57ad5bdb2c8b8116
Quintin Yu <quintin@arlanet.com>
2023-10-10 09:18:08 +0200

    Added home page ts for VUE

---
+++ Src/GMPA.Frontend/script/home.page.ts
@@ -0,0 +1,9 @@
+﻿import { createApp, pinia } from './vue';
+
+import HomeApp from './vue/apps/home';
+
+const homeAppMountingContainer = document.getElementById('home');
+
+if (homeAppMountingContainer) {
+    createApp(HomeApp).use(pinia).mount(homeAppMountingContainer);
+}
