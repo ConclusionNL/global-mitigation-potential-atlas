f601a4534d276d8cfcb127fad46a0f8e9a6864a9
Quintin Yu <quintin@arlanet.com>
2023-10-10 14:17:52 +0200

    Collapse all countries Bug: All continents close instead of one you clicked

--- Src/GMPA.Frontend/script/vue/apps/home/components/home-sidenav.vue
+++ Src/GMPA.Frontend/script/vue/apps/home/components/home-sidenav.vue
@@ -1,5 +1,6 @@
 ﻿<template>
     <ul class="countries-list show">
+        <!-- #region Pilot countries-->
         <li>
             <div class="country-divider">
                 Pilot
@@ -9,24 +10,25 @@
                 <a class="countries-link-active" :href=country.Url>{{country.Name}}</a>
             </div>
         </li>
-
         </li>
-        <li v-for="continent in continents">
+        <!-- #endregion -->
+
+        <li v-for="continent in continents" @click="toggleCollapse" :class="{'collapsed': isCollapsed}">
             <div class="country-divider">
                 {{continent}}
             </div>
-            <div v-for="country in props.countries">
-                <li v-if="country.Continent == continent">
-                    <div class="countries-list-item-active show">
-                        <div v-if="country.Active == 'True'">
-                            <a class="countries-link-active" :href=country.Url>{{country.Name}}</a>
-                        </div>
-                        <div v-else class="countries-list-item-disabled">
-                            {{country.Name}}
-                        </div>
-                    </div>
-                </li>
+            <div v-for="country in props.countries" v-show="isCollapsed" class="collapsible-content">
+        <li v-if="country.Continent == continent">
+            <div class="countries-list-item-active show">
+                <div v-if="country.Active == 'True'">
+                    <a class="countries-link-active" :href=country.Url>{{country.Name}}</a>
+                </div>
+                <div v-else class="countries-list-item-disabled">
+                    {{country.Name}}
+                </div>
             </div>
+        </li>
+        </div>
         </li>
     </ul>
 </template>
@@ -40,17 +42,23 @@
 
     const continents = ["Asia", "Africa", "North America", "South America", "Europe", "Oceania"]
 
+    const isCollapsed = ref(true);
+
+    const toggleCollapse = () => {
+        console.log(isCollapsed.value);
+        isCollapsed.value = !isCollapsed.value;
+    };
+
 </script>
 
 <style lang="scss">
-    .collapse {
-        &:not(.show) {
-            display: none;
-        }
+    .collapsible-content {
+        max-height: 0;
+        overflow: hidden;
+        transition: max-height 0.5s ease-in-out;
     }
 
-    .collapsing {
-        height: 0;
-        overflow: hidden;
+    .collapsed .collapsible-content {
+        max-height: 500px; /* Set an appropriate max-height value */
     }
 </style>
