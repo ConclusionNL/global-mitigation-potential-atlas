5ca5a23b44423fb64822b71f07394aae33a9d10a
Lucas Jellema <lucasjellema@gmail.com>
2023-11-15 16:19:40 +0100

    #93 text on top of stacked area chart to explain underlying model

--- Src/Frontend/gmpa-web/script/vue/apps/home/components/mitigation-potential-diagram.vue
+++ Src/Frontend/gmpa-web/script/vue/apps/home/components/mitigation-potential-diagram.vue
@@ -1,8 +1,15 @@
 ﻿<template>
     <h1>
         <span class="selected-collaboration">{{ selectedCollaboration }}</span
-        >'s Mitigation Potential Diagram 2030/2050
+        >'s Mitigation Potential Diagram for 2050
     </h1>
+    
+    <span class="model-explanation"  v-if="!(collaborationCountriesList[0]['DNM'])">
+    This uses a generalized STEVFNs model that better captures the challenges and benefits of international collaboration. This is translated from potentially different detailed national-level models used by respective countries. Emissions and costs are annualized for a 30 year greenfield project built in 2050. System meets 2050 demand for power and high temperature industrial heating sectors.
+    </span>
+    <span class="model-explanation"  v-else>
+        This uses a detailed national-level model that represents the current model and data being used by the country. This is used to calibrate the STEVFNs model used to model international collaboration. The system meets 2015-2070 demand for power, industry and transport sectors. The costs are the NPV costs for a brownfield project from 2015-2070. The emissions are 2050 emissions assuming a linear reduction from 2025-2050, and constant emissions from 2050-2070.
+    </span>
     <div v-if="countriesList.length > 0">
         <label for="countryDataSetSelect" v-if="countriesList.length > 1">Select one Country, Full collaboration or Autarky:</label>
         <label for="countryDataSetSelect" v-else>Select Overall or Detailed National Modelling:</label>
@@ -82,5 +89,10 @@ const dataSetTypeToShow = computed(() => {
 
 .select-element {
     width: auto; /* Allow the select element to adjust its width based on content */
+}
+
+.model-explanation {
+font-size:14px;
+font-style:italic
 }
 </style>
