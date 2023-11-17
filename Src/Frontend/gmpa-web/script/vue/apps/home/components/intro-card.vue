fb0f02f0f24441081f26026930794e3cd268aee8
Quintin Yu <quintin@arlanet.com>
2023-11-17 13:38:35 +0100

    Intro card size

--- Src/Frontend/gmpa-web/script/vue/apps/home/components/intro-card.vue
+++ Src/Frontend/gmpa-web/script/vue/apps/home/components/intro-card.vue
@@ -1,13 +1,12 @@
 <template>
     <div v-if="show" class="intro-card">
         <div class="content">
-            <div class="title">Welcome to the Global Mitigation Potential Atlas</div>
+            <div class="title">Welcome to the pilot of the Global Mitigation Potential Atlas! </div>
             <div class="instructions">
-                Welcome to the pilot of the Global Mitigation Potential Atlas! The Atlas is designed to inspire
-greater climate change mitigation ambition and implementation efforts, by presenting where
-significant and potentially transformative opportunities for reducing emissions are located,
-and what the key levers are for realising these opportunities. It fosters domestic action, as
-well as international collaboration and learning.
+                The Atlas is designed to inspire greater climate change mitigation ambition and implementation efforts, by presenting where 
+                significant and potentially transformative opportunities for reducing emissions are located,
+                and what the key levers are for realising these opportunities. It fosters domestic action, as
+                well as international collaboration and learning.
 <br/> <br/>
 We welcome your feedback on this pilot version for Southeast Asia – please get in touch
 with info@mitigationatlas.org
@@ -46,8 +45,8 @@ const handleExploreMapClick = () => {
     right: 0;
     top: 0;
     bottom: 0;
-    width: 867px;
-    height: 335px;
+    width: 1000px;
+    height: 400px;
     border-radius: 24px;
     padding: 40px;
     gap: 24px;
