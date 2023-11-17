6af039cf4763631ca7e33cd4ddfa227ab0b3e7b1
Lucas Jellema <lucasjellema@gmail.com>
2023-11-17 11:36:48 +0100

    #88 cater for country card and searchbar on smaller window sizes

--- Src/Frontend/gmpa-web/script/vue/apps/home/components/searchbar.vue
+++ Src/Frontend/gmpa-web/script/vue/apps/home/components/searchbar.vue
@@ -156,4 +156,14 @@ const onEnter = () => {
     color: red;
     font-weight: 500;
 }
+
+@media screen and (max-width: 1199px) {
+    .search-bar {
+    width: 235px;
+}
+
+.search-input .results {
+    width: 220px;
+}
+}
 </style>
